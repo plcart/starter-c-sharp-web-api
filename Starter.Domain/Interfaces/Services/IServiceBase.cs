2e2aff0230764409b0616170c2fa3e13f326e088
Arthur Ferreira <plc_4art@hotmail.com>
2016-06-27 22:58:26 -0300

    Base Repository methods

---
+++ Starter.Domain/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Starter.Domain.Interfaces.Services
+{
+    public interface IServiceBase<T> where T:class
+    {
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        T Get(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null);
+        IEnumerable<T> GetAll(Expression<Func<T, object>>[] includes = null, string order = "", bool reverse = false,
+            int skip = 0, int take = 0);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null,
+            string order = "", bool reverse = false, int skip = 0, int take = 0);
+        IEnumerable<T> GetAll(ref long count, Expression<Func<T, object>>[] includes = null, string order = "",
+            bool reverse = false, int skip = 0, int take = 0);
+        IEnumerable<T> GetAll(ref long count, Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null,
+            string order = "", bool reverse = false, int skip = 0, int take = 0);
+    }
+}
---
+++ Starter.Domain/Services/ServiceBase.cs
@@ -0,0 +1,66 @@
+﻿using Starter.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Starter.Domain.Interfaces.Repositories;
+
+namespace Starter.Domain.Services
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        internal protected IRepositoryBase<T> repository { get; }
+
+        public ServiceBase(IRepositoryBase<T> repo)
+        {
+            repository = repo;
+        }
+
+        public void Add(T entity)
+        {
+            repository.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            repository.AddRange(entities);
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null)
+        {
+            return repository.Get(predicate, includes);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, object>>[] includes = null, string order = "", bool reverse = false, int skip = 0, int take = 0)
+        {
+            return repository.GetAll(includes, order, reverse, skip, take);
+        }
+
+        public IEnumerable<T> GetAll(ref long count, Expression<Func<T, object>>[] includes = null, string order = "", bool reverse = false, int skip = 0, int take = 0)
+        {
+            return repository.GetAll(ref count, includes, order, reverse, skip, take);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null, string order = "", bool reverse = false, int skip = 0, int take = 0)
+        {
+            return repository.GetAll(predicate, includes, order, reverse, skip, take);
+        }
+
+        public IEnumerable<T> GetAll(ref long count, Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null, string order = "", bool reverse = false, int skip = 0, int take = 0)
+        {
+            return repository.GetAll(ref count, predicate, includes, order, reverse, skip, take);
+        }
+
+        public void Remove(T entity)
+        {
+            repository.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            repository.Update(entity);
+        }
+    }
+}
