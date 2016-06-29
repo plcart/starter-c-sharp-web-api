b5719f3fd521f1c19c45037290b892cc62dbc59d
arthur.ferreira <arthur.ferreira@NB-8934>
2016-06-29 10:36:56 -0300

    Validate model attribute

---
+++ Starter.Wep.Api/Filters/ValidateModelActionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http.Filters;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Linq;
+
+namespace Starter.Wep.Api.Filters
+{
+    public class ValidateModel : ActionFilterAttribute
+    {
+        string[] Names { get; }
+
+        public ValidateModel() { }
+
+        public ValidateModel(params string[] names)
+        {
+            Names = names;
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+                actionContext.Response = actionContext.Request
+                     .CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+            else if (actionContext.ActionArguments
+                .Where(x => Names.Contains(x.Key) && x.Value == null).Count() > 0)
+                actionContext.Response = actionContext.Request
+                    .CreateErrorResponse(HttpStatusCode.BadRequest, "The request is invalid.");
+        }
+
+    }
+}
