d4488bd8416b33c9ae6dfeca7935a88224a7c401
arthur.ferreira <arthur.ferreira@NB-8934>
2016-07-01 17:02:06 -0300

    Page title HTTP default methods

--- Starter.Wep.Api/Controllers/PageTitleController.cs
+++ Starter.Wep.Api/Controllers/PageTitleController.cs
@@ -1,4 +1,5 @@
-﻿using Starter.Domain.Entities;
+﻿using AutoMapper;
+using Starter.Domain.Entities;
 using Starter.Domain.Interfaces.Services;
 using Starter.Web.Api.Filters;
 using Starter.Web.Api.Models;
@@ -16,7 +17,7 @@ namespace Starter.Web.Api.Controllers
     {
         IDependencyScope resolver { get; } = GlobalConfiguration.Configuration.DependencyResolver.BeginScope();
         IServiceBase<PageTitle> pageService { get; }
-        static int Id = 0;
+
         public PageTitleController()
         {
             pageService = resolver.GetService(typeof(IServiceBase<PageTitle>)) as IServiceBase<PageTitle>;
@@ -26,16 +27,52 @@ namespace Starter.Web.Api.Controllers
         [Route("api/pages")]
         public IHttpActionResult Get()
         {
-            var s = new PageTitleModel() { Title = "sfasafa" , Id = Id };
-            return Ok(s);
+            var entities = pageService.GetAll();
+            return Ok(Mapper.Map<List<PageTitleModel>>(entities));
+        }
+
+        [HttpGet]
+        [Route("api/pages/{page}")]
+        public IHttpActionResult Get(Page page)
+        {
+            var entity = pageService.Get(p => p.Page == page);
+            return Ok(Mapper.Map<PageTitleModel>(entity));
         }
 
         [HttpPost]
         [Route("api/pages")]
-        public IHttpActionResult Post()
+        [ValidateModel("model")]
+        public IHttpActionResult Post(PageTitleModel model)
         {
-            var s = new PageTitleModel() { Title = "sfasafa", Id = Id++ };
-            return Ok(s);
+            var entity = Mapper.Map<PageTitle>(model);
+            pageService.Add(entity);
+            return Created($"api/pages/{entity.Page}", Mapper.Map<PageTitleModel>(entity));
+        }
+
+        [HttpPut]
+        [Route("api/pages/{page}")]
+        [ValidateModel("model")]
+        public IHttpActionResult Put(Page page,PageTitleModel model)
+        {
+            var entity = pageService.Get(p => p.Page == page);
+            return Ok(Mapper.Map<PageTitleModel>(entity));
+        }
+
+        [HttpDelete]
+        [Route("api/pages/{page}")]
+        public IHttpActionResult Delete(Page page)
+        {
+            var entity = pageService.Get(p => p.Page == page);
+            pageService.Remove(entity);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpDelete]
+        [Route("api/pages/")]
+        public IHttpActionResult Delete()
+        {
+            pageService.RemoveRange();
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
     }
