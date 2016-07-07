f1bd118a62be6e01ca7c74fc5f77ed7b81b6975c
arthur.ferreira <arthur.ferreira@NB-8934>
2016-07-07 15:55:49 -0300

    Auth repository and service default implementation

---
+++ Starter.Domain/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Starter.Domain.Entities;
+
+namespace Starter.Domain.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        void Register(User model);
+        User Login(string username, string password);
+    }
+}
---
+++ Starter.Infra.Data/Repositories/AuthRepository.cs
@@ -0,0 +1,22 @@
+﻿using Starter.Domain.Entities;
+using Starter.Domain.Interfaces.Repositories;
+using Starter.Infra.Data.Helpers.Extensions;
+
+namespace Starter.Infra.Data.Repositories
+{
+    public class AuthRepository : RepositoryBase<User>, IAuthRepository
+    {
+        public User Login(string username, string password)
+        {
+            var crypt = password.ToMD5();
+            var user = Get(x => x.Username == username && x.Password == password);
+            return user;
+        }
+
+        public void Register(User model)
+        {
+            model.Password = model.Password.ToMD5();
+            Add(model);
+        }
+    }
+}
